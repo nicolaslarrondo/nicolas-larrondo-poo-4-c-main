7f5134e2069c912a93414276554cb1561569f9bb
nicolaslarrondo <nicolaslarrondo@liceovvh.cl>
2024-04-04 14:24:01 -0300

    ok

---
+++ Fundamento JS/04_condisionales/scrip.js
@@ -0,0 +1,15 @@
+alert("hola")
+
+function identificar(){
+
+ let edad = prompt("hola");
+
+ if (edad >= 18) {
+    print(` es mayor de edad ${edad}`)
+ }
+ else
+ {
+    print(`no es mayor de edad ${edad}`)
+ }
+
+}
